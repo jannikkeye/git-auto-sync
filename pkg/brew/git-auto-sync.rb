83a8e17f5cf6f3415768b7b5767bbf87a0976ceb
Jannik Keye <jannik.keye@gmail.com>
2019-02-04 18:21:27 +0100

    fix formula

--- pkg/brew/git-auto-sync.rb
+++ pkg/brew/git-auto-sync.rb
@@ -4,8 +4,6 @@ class GitAutoSyncBin < Formula
     homepage "https://github.com/jannikkeye/git-auto-sync"
   
     url "https://janisis.ams3.digitaloceanspaces.com/gas"
-    end
-  
     conflicts_with "git-auto-sync"
   
     def install
