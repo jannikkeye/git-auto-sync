6e7240d933f79972f07ee7e871eeec86e10bef9a
Jannik Keye <jannik.keye@gmail.com>
2019-02-04 15:11:42 +0100

    fix: refactor main

--- src/bin/main.rs
+++ src/bin/main.rs
@@ -2,87 +2,34 @@ extern crate watchexec;
 extern crate clap;
 
 use git_auto_sync::{
-    repo
+    repo,
+    WatchHandler,
+    get_matches
 };
 use std::path;
-use clap::{Arg, App};
 use watchexec::{
     cli::Args,
-    pathop::PathOp,
-    error::Error,
-    run::{watch, Handler, ExecHandler}
+    run::watch
 };
 use std::env;
 
-struct WatchHandler {
-    inner: ExecHandler,
-}
-
-impl Handler for WatchHandler {
-    fn new(args: Args) -> Result<Self, Error> {
-        Ok(WatchHandler {
-            inner: ExecHandler::new(args.clone())?,
-        })
-    }
-
-    fn on_update(&mut self, ops: &[PathOp]) -> Result<bool, Error> {
-        self.start();
-
-        self.inner.on_update(ops)?;
-
-        Ok(true)
-    }
-
-    fn on_manual(&mut self) -> Result<bool, Error> {
-        self.start();
-
-        Ok(true)
-    }
-}
-
-impl WatchHandler {
-    fn start(&self) {
-        println!("Handling changes...");
-    }
-}
-
 fn main() {
-    let matches = App::new("My Super Program")
-        .version("1.0")
-        .author("Jannik Keye <jannik.keye@gmail.com>")
-        .about("Does awesome things")
-        .arg(Arg::with_name("path")
-            .help("Path to watch")
-            .index(1)
-            .default_value(".")
-            .required(true)
-        )
-        .arg(Arg::with_name("restart")
-            .help("Restart the process if it's still running")
-            .short("r")
-            .long("restart"))
-        .arg(Arg::with_name("debounce")
-            .help("Set the timeout between detected change and command execution, defaults to 500ms")
-            .takes_value(true)
-            .value_name("milliseconds")
-            .short("d")
-            .long("debounce"))
-        .get_matches();
+    let matches = get_matches();
 
     let path_arg = matches.value_of("path").unwrap();
-    let path = path::Path::new(&path_arg).canonicalize().unwrap();
+    let path = match path::Path::new(&path_arg).canonicalize() {
+        Ok(path) => path,
+        Err(err) => panic!(err),
+    };
 
-    env::set_current_dir(path.to_path_buf()).unwrap();
+    env::set_current_dir(path.to_path_buf()).expect(&format!("Failed to change current directory to {:#?}", &path));
 
     if path.is_dir() == false {
         panic!("{:?} is not a directory", path);
     }
 
     if repo::is_repo(&path) == false {
-        println!("{:#?} is not a git repository", path);
-        println!("Exiting...");
-
-        std::process::exit(1);
+        panic!(format!("{:#?} is not a git repository", path));
     }
 
 
--- src/lib.rs
+++ src/lib.rs
@@ -1,1 +1,54 @@
+use watchexec::{
+    cli::Args,
+    pathop::PathOp,
+    error::Error,
+    run::{Handler, ExecHandler}
+};
+use clap::{App, Arg};
+
 pub mod repo;
+
+pub struct WatchHandler {
+    inner: ExecHandler,
+}
+
+impl Handler for WatchHandler {
+    fn new(args: Args) -> Result<Self, Error> {
+        Ok(WatchHandler {
+            inner: ExecHandler::new(args.clone())?,
+        })
+    }
+
+    fn on_update(&mut self, ops: &[PathOp]) -> Result<bool, Error> {
+        self.start();
+
+        self.inner.on_update(ops)?;
+
+        Ok(true)
+    }
+
+    fn on_manual(&mut self) -> Result<bool, Error> {
+        self.start();
+
+        Ok(true)
+    }
+}
+
+impl WatchHandler {
+    fn start(&self) {
+        println!("Handling changes...");
+    }
+}
+
+pub fn get_matches<'a>() -> clap::ArgMatches<'a> {
+    App::new("My Super Program")
+        .version("1.0")
+        .author("Jannik Keye <jannik.keye@gmail.com>")
+        .about("Does awesome things")
+        .arg(Arg::with_name("path")
+            .help("Path to watch")
+            .index(1)
+            .required(true)
+        )
+        .get_matches()
+}
