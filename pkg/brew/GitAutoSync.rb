ccc519df6dfbbb791e72a9ec2468aa0bc87a99d7
Jannik Keye <jannik.keye@gmail.com>
2019-02-05 10:57:29 +0100

    feat: add formula

---
+++ pkg/brew/GitAutoSync.rb
@@ -0,0 +1,8 @@
+class GitAutoSync < Formula
+    version '0.1.3'
+    homepage "https://github.com/jannikkeye/git-auto-sync"
+    url "https://github.com/jannikkeye/git-auto-sync/releases/download/untagged-8faa1386578e5c4a1776/x86_64-apple-darwin.tar.gz"
+  
+    def install
+    end
+  end
